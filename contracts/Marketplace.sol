b30b40c6c155b4ddcbf8b996a26ee7f3257b1d1c
davydovMikhail <davydov27977@gmail.com>
2022-03-19 21:01:58 +0700

    general contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -10,9 +10,10 @@ contract Marketplace is Ownable {
     using SafeERC20 for IERC20;
     address private _address721;
     address private _address20;
+    uint256 private threeDays = 259200;
 
     mapping(uint256 => Deal) private _sales;
-    mapping(uint256 => Deal) private _auctions;
+    mapping(uint256 => Auction) private _auctions;
 
     struct Deal {
         address seller;
@@ -20,11 +21,13 @@ contract Marketplace is Ownable {
         bool active;
     }
 
-    // struct Auction {
-    //     uint256 startTime;
-    //     uint256 bidderCounter;
-    //     address lastBidder;
-    // }
+    struct Auction {
+        uint256 startTime;
+        uint256 bidderCounter;
+        address lastBidder;
+        uint256 price;
+        address seller;
+    }
 
     constructor(address address721_, address address20_) {
         _address721 = address721_;
@@ -44,18 +47,94 @@ contract Marketplace is Ownable {
     }
 
     function cancel(uint256 _tokenId) public {
+        require(
+            _sales[_tokenId].seller == msg.sender,
+            "the token does not belong to you"
+        );
         IERC721(_address721).transferFrom(address(this), msg.sender, _tokenId);
         _sales[_tokenId].active = false;
     }
 
     function buyItem(uint256 _tokenId) public {
         require(_sales[_tokenId].active, "deal no longer valid");
-        IERC20(_address20).transferFrom(
+        IERC20(_address20).safeTransferFrom(
             msg.sender,
             _sales[_tokenId].seller,
             _sales[_tokenId].price
         );
         IERC721(_address721).transferFrom(address(this), msg.sender, _tokenId);
         _sales[_tokenId].active = false;
+    }
+
+    // auction
+
+    function listItemOnAuction(uint256 _tokenId, uint256 _startPrice) public {
+        IERC721(_address721).transferFrom(msg.sender, address(this), _tokenId);
+        _auctions[_tokenId] = Auction(
+            block.timestamp,
+            0,
+            address(0),
+            _startPrice,
+            msg.sender
+        );
+    }
+
+    function makeBid(uint256 _tokenId, uint256 _newPrice) public {
+        require(
+            _auctions[_tokenId].startTime + threeDays > block.timestamp,
+            "auction time has passed"
+        ); // проверка на актуальность аукциона
+        require(
+            _auctions[_tokenId].price < _newPrice,
+            "your price is less than the current one"
+        ); // проверка на то что новая цена больше предыдущей
+        if (_auctions[_tokenId].bidderCounter > 0) {
+            IERC20(_address20).safeTransfer( // отправка токенов предыдущему участнику, который сделал ставку
+                _auctions[_tokenId].lastBidder,
+                _auctions[_tokenId].price
+            );
+        }
+        IERC20(_address20).safeTransferFrom( // перевод денего от нового участника торгов
+            msg.sender,
+            address(this),
+            _newPrice
+        );
+        _auctions[_tokenId].bidderCounter += 1;
+        _auctions[_tokenId].lastBidder = msg.sender;
+        _auctions[_tokenId].price = _newPrice;
+    }
+
+    function finishAuction(uint256 _tokenId) public {
+        require(
+            _auctions[_tokenId].startTime + threeDays < block.timestamp,
+            "the time for debriefing has not yet expired"
+        );
+        if (_auctions[_tokenId].bidderCounter > 2) {
+            IERC20(_address20).safeTransfer(
+                _auctions[_tokenId].seller,
+                _auctions[_tokenId].price
+            );
+            IERC721(_address721).transferFrom(
+                address(this),
+                _auctions[_tokenId].lastBidder,
+                _tokenId
+            );
+        } else if (_auctions[_tokenId].bidderCounter == 0) {
+            IERC721(_address721).transferFrom(
+                address(this),
+                _auctions[_tokenId].seller,
+                _tokenId
+            );
+        } else {
+            IERC20(_address20).safeTransfer(
+                _auctions[_tokenId].lastBidder,
+                _auctions[_tokenId].price
+            );
+            IERC721(_address721).transferFrom(
+                address(this),
+                _auctions[_tokenId].seller,
+                _tokenId
+            );
+        }
     }
 }
